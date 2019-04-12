aa398bf0f7cb4d54fc0039953d9f9a3a27fde71c
guodonghu <guodong_hu@126.com>
2019-04-12 11:36:39 +0800

    切片，字符串

---
+++ day4/ex8-slice2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func test() {
+	var a [5]int = [...]int{1, 3, 5, 7, 9}
+	b := a[1:]
+	fmt.Printf("s = %p, a[1] = %p \n", b, &a[1])
+
+	b[0] = 999
+	fmt.Println(a) // 由于b[0]和a[1]的地址相同，所以改变b[0]的值，a[1]也跟着改变了
+	// [1 999 5 7 9]
+
+	b = append(b, 10) // 又重新拷贝了一份， 根据容量的大小重新分配内存
+	fmt.Printf("s = %p, a[1] = %p \n", b, &a[1])
+}
+
+// 两个切片append，使用...
+func testAppend() {
+	var a = []int{123, 1234, 123, 123, 123}
+	var b = []int{45, 45, 4, 545, 45}
+	a = append(a, b...) // 两个切片append
+	fmt.Println(a)
+}
+
+func testCopy() {
+
+	var a = []int{123, 234, 34, 34, 34, 34, 3434}
+	b := make([]int, 1)
+	copy(b, a)
+	fmt.Println(b)
+
+}
+
+func main() {
+	testCopy()
+	testAppend()
+	test()
+}
---
+++ day4/ex9-string/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// string 底层是byte数组，可以进行slice操作
+func testString() {
+	str := "hhh eee"
+	s1 := str[0:3]
+	s2 := str[4:]
+	fmt.Println(s1)
+	fmt.Println(s2)
+}
+
+func testModifyString() {
+	s := "hello world"
+	//	s[1] = 'E' // 字符串是不可变的
+	s1 := []byte(s) // 转换成byte数组，是可变的了
+	s1[0] = 'X'
+	str := string(s1)
+	fmt.Println(str)
+
+	s = "我在深圳，hello"
+	s2 := []rune(s)
+	s2[0] = '你'
+	s = string(s2)
+	fmt.Println(s)
+
+}
+
+func main() {
+	testModifyString()
+	testString()
+}
