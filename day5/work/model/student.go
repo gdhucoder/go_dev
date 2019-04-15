00e15c15f5e6a9402deaa2405fddb401ad779d9a
guodonghu <guodong_hu@126.com>
2019-04-15 16:34:58 +0800

    book

--- day5/work/model/student.go
+++ day5/work/model/student.go
@@ -4,9 +4,9 @@ type Student struct {
 	Id            string // 学号
 	Name          string //
 	Grade         int
-	IDNum         string      // 身份证号
-	Sex           int         // 0 female 1 male
-	BooksBorrowed []book.Book // 借了几本书
+	IDNum         string // 身份证号
+	Sex           int    // 0 female 1 male
+	BooksBorrowed []Book // 借了几本书
 }
 
 func InitStudent() []Student {
