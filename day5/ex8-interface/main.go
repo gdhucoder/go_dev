46088b2aab9a85b8a965ef92d454d0093c0a5a91
guodonghu <guodong_hu@126.com>
2019-04-15 13:59:05 +0800

    接口

--- day5/ex8-interface/main.go
+++ day5/ex8-interface/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Test interface {
 	Print()
+	Sleep()
 }
 
 type Student struct {
@@ -12,15 +13,49 @@ type Student struct {
 }
 
 func (p *Student) Print() {
-	fmt.Println(p)
+	fmt.Println("student: ", p)
+}
+
+func (p *Student) Sleep() {
+	fmt.Println("student sleep")
+}
+
+type People struct {
+	Name string
+	Age  int
+}
+
+func (p *People) Print() {
+	fmt.Println("people: ", p)
+}
+
+func (p *People) Sleep() {
+	fmt.Println("people sleep")
 }
 
 func main() {
+	var t Test
+	t.Print() // 接口就是一个地址（指针）
 	var stu Student
 	stu.Name = "huguodong"
 	stu.Age = 33
 
-	var t Test
 	t = &stu // 接口可以指向这个类型
 	t.Print()
+	t.Sleep()
+
+	var p People = People{
+		Name: "wtw",
+		Age:  1,
+	}
+
+	t = &p
+	t.Print()
+	t.Sleep()
+
+	// student:  &{huguodong 33}
+	// student sleep
+	// people:  &{wtw 1}
+	// people sleep
+
 }
