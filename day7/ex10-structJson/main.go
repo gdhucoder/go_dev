7a10206ba405d6d79b143a470648c5093f85be90
guodonghu <guodong_hu@126.com>
2019-04-21 09:14:41 +0800

    序列化

--- day7/ex10-structJson/main.go
+++ day7/ex10-structJson/main.go
@@ -6,14 +6,29 @@ import (
 )
 
 type User struct {
-	Name     string
+	Name     string `json:"username"`
 	NickName string
 	Age      int
 	Phone    string
 }
 
-// json序列化结构体
-func main() {
+func testMap() {
+	mp := make(map[string]interface{})
+	mp["john"] = 100
+	mp["dragon"] = 1091
+	mp["mother"] = "hero"
+	mp["jiongen"] = 12390123.20
+	u := User{
+		Name:     "mahuateng",
+		NickName: "tod",
+		Age:      18,
+		// Phone:    "110",
+	}
+	mp["user"] = u
+	jsonMarshal(mp)
+}
+
+func testStruct() {
 	u := User{
 		Name:     "mahuateng",
 		NickName: "tod",
@@ -21,12 +36,54 @@ func main() {
 		// Phone:    "110",
 	}
 
-	data, err := json.Marshal(u)
+	jsonMarshal(u)
+}
+
+func jsonMarshal(data interface{}) {
+	info, err := json.Marshal(data)
 
 	if err != nil {
 		fmt.Printf("json marshal failed, error :%s\n", err)
 	}
 
-	fmt.Println(string(data))
+	fmt.Println(string(info))
+}
+
+func testSlice() {
+	var s []map[string]interface{}
+	mp := make(map[string]interface{})
+	mp["john"] = 100
+	mp["dragon"] = 1091
+	mp["mother"] = "hero"
+	mp["jiongen"] = 12390123.20
+	u := User{
+		Name:     "mahuateng",
+		NickName: "tod",
+		Age:      18,
+		// Phone:    "110",
+	}
+	mp["user"] = u
+	s = append(s, mp)
+	mp = make(map[string]interface{})
+	mp["john"] = 10011
+	mp["dragon"] = 109111
+	mp["mother"] = "hero11"
+	s = append(s, mp)
+	jsonMarshal(s)
+
+}
+
+// json序列化结构体
+func main() {
+	testStruct()
 	// {"Name":"mahuateng","NickName":"tod","Age":18,"Phone":""}
+	// 使用json tag
+	// {"username":"mahuateng","NickName":"tod","Age":18,"Phone":""}
+
+	testMap()
+	// {"dragon":1091,"john":100}
+
+	testSlice()
+	// [{"dragon":1091,"jiongen":12390123.2,"john":100,"mother":"hero","user":{"username":"mahuateng","NickName":"tod","Age":18,"Phone":""}},
+	// {"dragon":109111,"john":10011,"mother":"hero11"}]
 }
