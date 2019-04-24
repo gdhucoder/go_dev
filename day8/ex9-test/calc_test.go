b532c4c0b7c1255a835aa7174b9bcae56b590cdc
guodonghu <guodong_hu@126.com>
2019-04-24 16:13:44 +0800

    test

--- day8/ex9-test/calc_test.go
+++ day8/ex9-test/calc_test.go
@@ -3,9 +3,27 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	r := add(3, 4)
+	r := add(2, 4)
 	if r != 6 {
 		t.Fatalf("expecting %d, but %d\n", 6, r)
+	}
+	t.Logf("test succ")
+
+	// PS D:\project> cd .\src\go_dev\day8
+	// PS D:\project\src\go_dev\day8> cd .\ex9-test\
+	// PS D:\project\src\go_dev\day8\ex9-test> go test -v
+	// === RUN   TestAdd
+	// --- PASS: TestAdd (0.00s)
+	// 		calc_test.go:10: test succ
+	// PASS
+	// ok      go_dev/day8/ex9-test    0.506s
+	// PS D:\project\src\go_dev\day8\ex9-test>
+}
+
+func TestSub(t *testing.T) {
+	r := add(3, 4)
+	if r != -1 {
+		t.Fatalf("expecting %d, but %d\n", -1, r)
 	}
 	t.Logf("test succ")
 
