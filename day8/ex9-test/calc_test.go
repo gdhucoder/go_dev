54a46557787a28861a736dc4be208f11cab76e11
guodonghu <guodong_hu@126.com>
2019-04-24 16:05:27 +0800

    go如何写测试案例

---
+++ day8/ex9-test/calc_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	r := add(2, 4)
+	if r != 6 {
+		t.Fatalf("expecting %d, but %d\n", 6, r)
+	}
+	t.Logf("test succ")
+
+	// PS D:\project> cd .\src\go_dev\day8
+	// PS D:\project\src\go_dev\day8> cd .\ex9-test\
+	// PS D:\project\src\go_dev\day8\ex9-test> go test -v
+	// === RUN   TestAdd
+	// --- PASS: TestAdd (0.00s)
+	// 		calc_test.go:10: test succ
+	// PASS
+	// ok      go_dev/day8/ex9-test    0.506s
+	// PS D:\project\src\go_dev\day8\ex9-test>
+}
