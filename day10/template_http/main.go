fd7096dbab08d4cbdbeb80ab1fb9e612b0aa6c86
guodonghu <guodong_hu@126.com>
2019-04-26 16:25:07 +0800

    template

--- day10/template_http/main.go
+++ day10/template_http/main.go
@@ -11,16 +11,46 @@ var (
 	myTemplate *template.Template
 )
 
+type Result struct {
+	output string
+}
+
+func (p *Result) Write(b []byte) (n int, err error) {
+	fmt.Println("called by template")
+	p.output += string(b)
+	return len(b), nil
+}
+
 type Person struct {
 	Name  string
 	Title string
-	age   string
+	Age   int
 }
 
 func Info(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("handle login")
-	p := Person{Name: "胡国栋", Title: "Engineer", age: "31"}
-	myTemplate.Execute(w, p)
+	fmt.Println("handle info")
+	p := Person{Name: "胡国栋", Title: "Engineer", Age: 17}
+	myTemplate.Execute(w, p) // w实现了写的接口
+	resultWriter := &Result{}
+	myTemplate.Execute(resultWriter, p)
+	fmt.Println(resultWriter.output) // 自己实现Write接口
+
+	// called by template
+	// called by template
+	// called by template
+	// called by template
+	// called by template
+	// called by template
+	// called by template
+	// <html>
+	// 	<head>
+	// 		<title>Engineer</title>
+	// 	</head>
+	// 	<body>
+	// 		<h1>Hello, 胡国栋</h1>
+	// 		{胡国栋 Engineer 31}
+	// 	</body>
+	// </html>
 }
 
 func initTemplate(filename string) (err error) {
@@ -35,7 +65,7 @@ func initTemplate(filename string) (err error) {
 func main() {
 
 	// 初始化模板
-	initTemplate("D:\\project\\src\\go_dev\\day10\\template\\index.html")
+	initTemplate("D:\\project\\src\\go_dev\\day10\\template_http\\index.html")
 
 	// 处理请求
 	http.HandleFunc("/user/info", Info)
