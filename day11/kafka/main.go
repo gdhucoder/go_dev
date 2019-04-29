83d6d245d7eeed7e6d3b3faea3a15a324891493d
guodonghu <guodong_hu@126.com>
2019-04-29 23:38:52 +0800

    update

---
+++ day11/kafka/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+
+	client, err := sarama.NewSyncProducer([]string{"192.168.31.224:9092"}, config)
+	if err != nil {
+		fmt.Println("producer close, err:", err)
+		return
+	}
+
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "nginx_log"
+	msg.Value = sarama.StringEncoder("胡国栋")
+
+	defer client.Close()
+
+	pid, offset, err := client.SendMessage(msg)
+	if err != nil {
+		fmt.Println("send message failed,", err)
+		return
+	}
+
+	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+}
